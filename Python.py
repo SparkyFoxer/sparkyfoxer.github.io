ef287708f8c85716c2c4704c3c07fa66a83fa4ed
Sparky Fox <166080464+SparkyFoxer@users.noreply.github.com>
2025-07-23 10:52:14 +1200

    Python.py

--- Python.py
+++ Python.py
@@ -1,4 +1,5 @@
 # this is a comment
 # this file im going to learn how to put comments and use the print function
 
-print ("Hello World :D") # This is an inline comment
+print ("Hello World :D")                        # This is an inline comment
+"""hallaur?"""                                  # I don't remember what this is called xD
