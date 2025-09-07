17d9f1ebefeaca58ab8aeeb929985728579f0b25
Sparky Foxer <brendanchapman288@gmail.com>
2025-09-08 11:44:21 +1200

    more stuff

--- PYTHONNNNNN/week 8 Mon.py
+++ PYTHONNNNNN/week 8 Mon.py
@@ -16,11 +16,46 @@
 #     else:
 #         #CODE
 
-Num = int(input("Pleas enter a number: "))
-if Num >=0:
-    if Num == 0:
-        print("The number is 0")
-    else:
-        print("The number is positive")
-else:
-    print("The number is negative.")
+# Num = int(input("Pleas enter a number: "))
+# if Num >=0:
+#     if Num == 0:
+#         print("The number is 0")
+#     else:
+#         print("The number is positive")
+# else:
+#     print("The number is negative.")
+
+# Word=input("Give me a word: ")
+# CheckedWord=Word.isupper()
+# print(CheckedWord) # is checking to see if the word inputed is all caps or not
+
+# Word=(input("Please input something: ")).strip()
+# CheckedWord=Word.isalpha()
+# if Word.isalpha("a")
+#     if CheckedWord.startswith("a"):
+#         print("Starts with a")
+#     elif CheckedWord.startswith("a"):
+#         print("Doesnt start with a")
+# else:
+#     print("You put a mix of characters")
+
+# Test = int(input("What percent did you get for your test result?: "))
+# if Test <0:
+#     print("Incorrect Statement")
+#     if Test <50 and Test >0:
+#         print("That is a fail")
+#     elif Test <=50 and Test >90:
+#             print("You get a B")
+#     elif Test <=90 and Test >100:
+#             print("You get a A")
+#     elif Test == 100:
+#         print("You get a perfect grade!")
+# else:
+#     print("ERROR")
+
+
+Number=int(input("Please enter a number: ")).strip()
+"This is an invalid number"
+if Number "The number is even."
+elif"The number is odd."
+print("Thank you for your input.")
