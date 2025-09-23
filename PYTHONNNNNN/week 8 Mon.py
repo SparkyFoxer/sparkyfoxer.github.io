c2eef57f886ded5fe1a97f477bed495b33587fe1
SparkyFoxer <sparkyfoxer@gmail.com>
2025-09-23 15:46:14 +1200

    yea

--- PYTHONNNNNN/week 8 Mon.py
+++ PYTHONNNNNN/week 8 Mon.py
@@ -8,13 +8,13 @@ if Name == "Brendan" or Age == 20:
 else:
     print("none of the statements are True")
 
-Num1=str(input("Number1: "))
+Num1=int(input("Number1: "))
 if Num1 >= 0:
     if Num1 == 0:
+        pass
     else:
         #CODE
-    else:
-        #CODE
+        pass
 
 Num = int(input("Pleas enter a number: "))
 if Num >=0:
@@ -29,24 +29,24 @@ Word=input("Give me a word: ")
 CheckedWord=Word.isupper()
 print(CheckedWord) # is checking to see if the word inputed is all caps or not
 
-Word=(input("Please input something: ")).strip()
-CheckedWord=Word.isalpha()
-if Word.isalpha("a")
-    if CheckedWord.startswith("a"):
-        print("Starts with a")
-    elif CheckedWord.startswith("a"):
-        print("Doesnt start with a")
-else:
-    print("You put a mix of characters")
+# Word=(input("Please input something: ")).strip()
+# CheckedWord=Word.isalpha()
+# if CheckedWord:
+#     if CheckedWord.startswith("a"):
+#         print("Starts with a")
+#     elif CheckedWord.startswith("a"):
+#         print("Doesnt start with a")
+# else:
+#     print("You put a mix of characters")
 
 Test = int(input("What percent did you get for your test result?: "))
 if Test <0:
     print("Incorrect Statement")
     if Test <50 and Test >0:
         print("That is a fail")
-    elif Test <=50 and Test >90:
+    elif Test >=50 and Test <90:
             print("You get a B")
-    elif Test <=90 and Test >100:
+    elif Test >=90 and Test <100:
             print("You get a A")
     elif Test == 100:
         print("You get a perfect grade!")
@@ -54,8 +54,8 @@ else:
     print("ERROR")
 
 
-Number=int(input("Please enter a number: ")).strip()
-"This is an invalid number"
-if Number "The number is even." # UFINISHED
-elif"The number is odd."
-print("Thank you for your input.")
+# Number=int(input("Please enter a number: ")).strip()
+# "This is an invalid number"
+# if Number "The number is even." # UFINISHED
+# elif"The number is odd."
+# print("Thank you for your input.")
