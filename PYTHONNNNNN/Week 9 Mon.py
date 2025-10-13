b8cc4fc2142a818896ab677ea4802e8ab7ded5b7
Sparky Foxer <brendanchapman288@gmail.com>
2025-10-14 09:04:48 +1300

    committ

--- PYTHONNNNNN/Week 9 Mon.py
+++ PYTHONNNNNN/Week 9 Mon.py
@@ -1,26 +1,35 @@
-# 15/09/2025
-# Lists and if
+# # 15/09/2025
+# # Lists and if
 
-# lists do not need to be in any order eg. ["cat","dog","25","27"]
-ShoppingList=["Toothpaste","Tomato sauce","Lettuce","Buns","Chips","Noodles"]
-List=len(ShoppingList) # Print out how many items are there in the list. Hint: use len() 
-print(ShoppingList[0]) # Print out the first list item. (Toothpaste)
-print(ShoppingList[1:4]) # Print out the second, third and fourth item in the list (Tomato sauce, Lettuce and Buns)
-print(ShoppingList[-1]) # Print out the last item in the list  Hint: use index [-1] (Noodles)
-print(ShoppingList[0:3]) # Print the items from the beginning of our list to “Buns”, but not including Buns. (Toothpaste, Tomato sauce, Lettuce)
-print(ShoppingList[2:6]) # Print the items from “Lettuce” to the end of the list. (Lettuce, Buns, Chips, Noodles)
+# # lists do not need to be in any order eg. ["cat","dog","25","27"]
+# ShoppingList=["Toothpaste","Tomato sauce","Lettuce","Buns","Chips","Noodles"]
+# List=len(ShoppingList) # Print out how many items are there in the list. Hint: use len() 
+# print(ShoppingList[0]) # Print out the first list item. (Toothpaste)
+# print(ShoppingList[1:4]) # Print out the second, third and fourth item in the list (Tomato sauce, Lettuce and Buns)
+# print(ShoppingList[-1]) # Print out the last item in the list  Hint: use index [-1] (Noodles)
+# print(ShoppingList[0:3]) # Print the items from the beginning of our list to “Buns”, but not including Buns. (Toothpaste, Tomato sauce, Lettuce)
+# print(ShoppingList[2:6]) # Print the items from “Lettuce” to the end of the list. (Lettuce, Buns, Chips, Noodles)
 
-ShoppingList.append("Milk") # Add “Milk” to the end of the list
-ShoppingList.insert(0,"Chocolate") # Add Chocolate to the beginning of your list as a new item
-ShoppingList.insert(6,"Apples") # Add this item between “Chips” and “Noodles”
-ShoppingList.append("Bread")
+# ShoppingList.append("Milk") # Add “Milk” to the end of the list
+# ShoppingList.insert(0,"Chocolate") # Add Chocolate to the beginning of your list as a new item
+# ShoppingList.insert(6,"Apples") # Add this item between “Chips” and “Noodles”
+# ShoppingList.append("Bread")
 
-ShoppingList[3]="Bananas"
-ShoppingList[2]="Ice Cream"
-ShoppingList.append("Honey")
-ShoppingList.append(26.50)
-ShoppingList.remove("Chips")
-ShoppingList.remove("Toothpaste")
-List=len(ShoppingList)
-print(ShoppingList)
-print(List)
+# ShoppingList[3]="Bananas"
+# ShoppingList[2]="Ice Cream"
+# ShoppingList.append("Honey")
+# ShoppingList.append(26.50)
+# ShoppingList.remove("Chips")
+# ShoppingList.remove("Toothpaste")
+# List=len(ShoppingList)
+# print(ShoppingList)
+# print(List)
+
+#BrendanC
+#Question12
+
+colours = ["blue", "yellow", "red", "pink", "purple", "green", "black", "white"]
+print(colours)
+print(colours[3:7])
+colours[1]="orange"
+print(colours[0:7])
--- PYTHONNNNNN/Week 9 Wed - CODERUNNER Q.py
+++ PYTHONNNNNN/Week 9 Wed - CODERUNNER Q.py
@@ -1,12 +1,12 @@
-# 17/09/2025
-# Basic loops (Using if() witth lists[])
+# # 17/09/2025
+# # Basic loops (Using if() witth lists[])
 
 # fruits = ["apple", "banana","cherry"]
 # for Eachfruit in fruits:
 #     print(Eachfruit)
 
-# for "amount" in fruits, print how many times the "amount" of the item is in the list
-# is _apple_ in fruits = yes = "print apple"
+# # for "amount" in fruits, print how many times the "amount" of the item is in the list
+# # is _apple_ in fruits = yes = "print apple"
 
 # Animals = ["Cat","Dog","Bird","Fox","Mouse"] # my list
 # print (Animals) # print the list
@@ -49,44 +49,44 @@
 # print(Food)
 # print(f"Your list of favourite foods are {FavFood}, {NextFavFood}, {AnotherFood}!")
 
-# #BrendanC
-# #Question4
+# # #BrendanC
+# # #Question4
 
 # grocery_list = ["milk", "bread", "eggs", "butter", "apples", "cheese"]
 # grocery_list[3] = "cereal"
 # print(grocery_list)
 
-#BrendanC
-#Question5
+# #BrendanC
+# #Question5
 
 # grocery_list = ["milk", "bread", "eggs", "butter", "apples", "cheese"]
 # grocery_list.append("chips")
 # grocery_list.append("pies")
 # print(grocery_list)
 
-# #BrendanC
-# #Question8
+# # #BrendanC
+# # #Question8
 
 # grocery_list = ["milk", "bread", "eggs", "butter", "apples", "cheese", "chips", "vegemite", "peanut butter", "chicken"]
 # List=len(grocery_list)
 # print(List)
 
-#BrendanC
-#Question5
-
-#Enter the temperature in °C:
-#Is it raining? (yes/no) 
-
-# If it’s 30 or above and not raining, print "Wear shorts and a t-shirt".
-# If it’s 20–29 and not raining, print "Wear light clothes".
-# If it’s 20–29 and raining, print "Wear light clothes and take an umbrella".
-# If it’s 10–19 and not raining, print "Wear a sweater".
-# If it’s 10–19 and raining, print "Wear a sweater and raincoat".
-# If it’s below 10  print "Extreme weather – stay indoors!".  
-
-
 # #BrendanC
 # #Question5
+
+# #Enter the temperature in °C:
+# #Is it raining? (yes/no) 
+
+# # If it’s 30 or above and not raining, print "Wear shorts and a t-shirt".
+# # If it’s 20–29 and not raining, print "Wear light clothes".
+# # If it’s 20–29 and raining, print "Wear light clothes and take an umbrella".
+# # If it’s 10–19 and not raining, print "Wear a sweater".
+# # If it’s 10–19 and raining, print "Wear a sweater and raincoat".
+# # If it’s below 10  print "Extreme weather – stay indoors!".  
+
+
+# # #BrendanC
+# # #Question5
 # Temp=int(input("Enter the temperature in °C: "))
 # Raining=input("Is it raining? (yes/no) ").strip().lower()
 # if Raining == "yes":
@@ -99,8 +99,8 @@
 #     elif Temp >=10 and Temp <=19: print("Wear a sweater")
 #     elif Temp <10: print("Extreme weather - stay indoors!")
 
-# #BrendanC
-# #Question6
+# # #BrendanC
+# # #Question6
 
 # Word=input("What is your word, number or phrase? ").strip()
 # if Word.isalpha():
@@ -115,32 +115,41 @@
 #     print("Your word is a mix of other characters, including punctuation.")
 
 
-# #BrendanC
-# #Question7
+# # #BrendanC
+# # #Question7
 
-# Word=input("Enter a word: ").strip().capitalize()
-# if Word.startswith("A"):
-#     print("The word begins with A or a.")
-# elif Word.startswith("B"):
-#     print("The word begins with B or b.")
-# else:
-#     print("The word begins with something else.")
+# # Word=input("Enter a word: ").strip().capitalize()
+# # if Word.startswith("A"):
+# #     print("The word begins with A or a.")
+# # elif Word.startswith("B"):
+# #     print("The word begins with B or b.")
+# # else:
+# #     print("The word begins with something else.")
 
-# #BrendanC
-# #Question8
-# # It is {} that the word {} starts with the letter {}.
+# # #BrendanC
+# # #Question8
+# # # It is {} that the word {} starts with the letter {}.
 
-# Letter=str(input("Give me a letter of the alphabet: ")).strip().capitalize()
-# AlphaWord=str(input(f"Please enter a word that starts with {Letter}: ")).strip().capitalize()
-# if AlphaWord.startswith(Letter):
-#     print(f"It is True that the word {AlphaWord} starts with the letter {Letter}.")
-# else:
-#     print(f"It is False that the word {AlphaWord} starts with the letter {Letter}.")
+# # Letter=str(input("Give me a letter of the alphabet: ")).strip().capitalize()
+# # AlphaWord=str(input(f"Please enter a word that starts with {Letter}: ")).strip().capitalize()
+# # if AlphaWord.startswith(Letter):
+# #     print(f"It is True that the word {AlphaWord} starts with the letter {Letter}.")
+# # else:
+# #     print(f"It is False that the word {AlphaWord} starts with the letter {Letter}.")
 
-# BrendanC
-# Question9
+# # BrendanC
+# # Question9
 
-Word=input("Please enter a word whose letters are all lower case: ")
+# Word=input("Please enter a word whose letters are all lower case: ")
 
-#It is {} that you have entered a lower case word
-#It is {}, Try again
+# #It is {} that you have entered a lower case word
+# #It is {}, Try again
+
+
+#BrendanC
+#Question 6
+
+Num = int(input("Please enter a number: "))
+Ans = ({Num} - 1)
+print("Please re-enter the number, zero or negative numbers are not allowed")
+print(f"The answer to {Num} - 1 is {Ans}")
