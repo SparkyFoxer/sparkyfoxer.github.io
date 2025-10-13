73135ae2f5d3ebb183e396ae9c31eb689abb8c4e
Sparky Foxer <brendanchapman288@gmail.com>
2025-10-14 09:04:31 +1300

    commit

---
+++ PYTHONNNNNN/Week 10 Mon.py
@@ -0,0 +1,77 @@
+# 6/10/2025
+# Pseudocode practice
+# Can I go to the beach today?
+
+# Temp=int(input("What is the current temp?: "))
+# Day=input("What day of the week is it? Mon, Tue, Wed, Thu, Fri, Sat, Sun: ").strip().capitalize()
+# if Day=="Sat" or Day=="Sun":
+#     if Temp >= 20 and Temp <= 30: print("Yes you can go to the beach!")
+#     else: print("You can't go to the beach")
+# else: print("You can not go to the beach")
+
+
+# word = input("Please enter a word whose letters are all lower case: ").strip()
+# if word.islower():
+#     print(f"It is {True} that you have entered a lower case word")
+# else:
+#     print(f"It is {False}, Try again")
+
+# #Brendan C
+# #Question 1
+
+
+# Number=int(input("Please enter a number: "))
+# if Number <= 0:
+#     print("This is an invalid number")
+# elif (Number % 2):
+#     print ("The number is odd.")
+# else:
+#     print("The number is even.")
+# print("Thank you for your input.")
+
+# # Brendan C
+# # Question 2
+
+# Number=float(input("Please enter a number: "))
+# if (Number <= 0):
+#     print("Invalid Number - Try again!")
+# elif (Number % 5 == 0):
+#     print(f"The number {Number} is divisible by 5.")
+# else:
+#     print(f"The number {Number} is not a multiple of 5.")
+
+# Brendan C
+# Question 3
+
+# Password=str(input("Please enter the password: "))
+# Password == "sunbeam"
+# if Password != "sunbeam":
+#     print("NO ACCESS")
+# else:
+#     print("ENTRY GRANTED")
+
+# The lines of text you need to use to generate the output are given below for you.
+# Use f-strings to generate the output.
+
+# integer = input(f'Please enter an integer: ')
+# ans = int(integer) + 1
+# print (f'The answer to {integer} + 1 is {ans}')
+
+# #BrendanC
+# #question 4
+
+# num = int(input("Please enter an integer: "))
+# print(f"Hi, you entered {num} which is of type {type(num)}")
+
+# Is the result of this code True or False?
+
+
+
+greeting = len("Hello World")
+print (greeting)
+
+# What number will we get as output?
+
+
+
+
---
+++ PYTHONNNNNN/Week 10 Wed.py
@@ -0,0 +1,5 @@
+#BrendanC
+#Question2
+
+MyList=[2,5,6,9,10,25,13]
+print(MyList)
